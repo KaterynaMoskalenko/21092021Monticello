ef4d0fa5c1b03ac80ed3adcc98b57dc696e14f6b
KaterynaMoskalenko <emochkaom@gmail.com>
2021-09-19 17:58:05 +0300

    Added slider

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use.strict';
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        speed: 2500,
+        slidesToShow: 3,
+        infinite: false,
+        
+    });
+  });
+
